019b47526e6750ea42325e0a4b65a6950cf8c1de
tkdguy85 <dallas.gordon3@gmail.com>
2021-03-04 16:54:33 -0600

    Auth page scripts added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './App.css';
+import LoginButton from './components/loginButton'
+import LogoutButton from './components/logoutButton'
+import Profile from './components/profile';
+import { useAuth0 } from '@auth0/auth0-react'
+
 import RosterList from './components/rosterList'
 
 function App() {
+  const { isLoading } = useAuth0()
+
+  if (isLoading) return <div>Loading...don't try to read all of this.</div>
   return (
-   <RosterList />
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+      <RosterList />     
+    </div>
+   
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import RosterReducer from './reducers/rosterReducer'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 if(localStorage.getItem('roster') == null)
   localStorage.setItem('roster', JSON.stringify([]))
@@ -15,10 +19,20 @@ let initialState = {
 }
 const store = createStore(RosterReducer, initialState)
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+
+
+ReactDOM.render(  
+  
+  <Auth0Provider 
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  > 
+    <Provider store={store}> 
+      <App />
+    </Provider>
+  </Auth0Provider>
+  ,
   document.getElementById('root')
 );
 
